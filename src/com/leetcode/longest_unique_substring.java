91d7609d840ba5b35f452283b9ff9e1ecc24317b
Nilamadhab Mohanty <nilamadhavmohanty@gmail.com>
2022-08-16 11:25:51 -0700

    longest_unique_substring : Added comments for the solution

--- src/com/leetcode/longest_unique_substring.java
+++ src/com/leetcode/longest_unique_substring.java
@@ -9,18 +9,20 @@ public class longest_unique_substring {
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        int start = 0;
-        int end = 0;
-        int maxSize = 0;
+        int start = 0; //Pointer for begining
+        int end = 0; // Pointer to move till the end
+        int maxSize = 0; //hold the final output
 
-        HashSet<Character> countOfChars = new HashSet<Character>();
-        while(end<s.length())
+        HashSet<Character> countOfChars = new HashSet<Character>(); // Hashset to check uniqueness of char
+        while(end<s.length()) // till end reaches the end of the string
         {
+            // if char is found, uniqueness violated; increase start index and remove the char at the start
             if(countOfChars.contains(s.charAt(end)))
             {
                 countOfChars.remove(s.charAt(start));
                 start++;
             }
+            // if not found proceed , add the end char to hashset and proceed with maxSize updation
             else
             {
                 countOfChars.add(s.charAt(end));
